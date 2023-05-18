100217aca46a697992ee2bb760f0218d625ef75f
Vivek-Chaudhari30 <vivek.cs.study@gmail.com>
2023-05-18 22:22:07 +0530

    Create frontpage.cpp

---
+++ frontpage.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+//now I am confused ke how to code chess in C++
+//bcoz I only knoe C++
+class frontpage
+{
+    private:
+    int a;
+
+    public:
+    int b;
+
+
+}
+
+
+int main()
+{
+    
+    
+    
+    return 0;
+}
